86d78967db6721f470039d09df678684928bed85
ali <a.korovkin@distillery.com>
2020-12-24 22:03:27 +0400

    Initial Commit

---
+++ ScorpionChart/ContentView.swift
@@ -0,0 +1,21 @@
+//
+//  ContentView.swift
+//  ScorpionChart
+//
+//  Created by Alelksandr Korovkin on 24.12.2020.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        Text("Hello, world!")
+            .padding()
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
---
+++ ScorpionChart/ScorpionChartApp.swift
@@ -0,0 +1,17 @@
+//
+//  ScorpionChartApp.swift
+//  ScorpionChart
+//
+//  Created by Alelksandr Korovkin on 24.12.2020.
+//
+
+import SwiftUI
+
+@main
+struct ScorpionChartApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
